e9a389217d35b208ec072ee5fd24cc0cf9b322c8
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-10-03 13:10:20 +0100

    Formulário de ecição de perfil validado

---
+++ assets/js/profile.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#bnt-edit-profile").click(function () {
+        const isNameValid = validateName("name", "name-err");
+        const isEmailValid = validateEmail("email", "email-err");
+        const isBirthdateValid = validateBirthdate("birthdate", "birthdate-err");
+        const isGenderValid = validateGender("gender", "gender-err");
+        const isValidPhone = validatePhone("phone", "phone-err");
+    });
+});
