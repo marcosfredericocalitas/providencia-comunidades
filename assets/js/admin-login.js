81aa6b8b9529327cfab9875a39033774b23e9098
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-10-05 01:32:49 +0100

    Login do admin criado

---
+++ assets/js/admin-login.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    alert('msg');
+    // Validation by blur (Focusout event)
+
+    // Validation by click button (Click event)
+    $("#btn-login").click(function () {
+        alert('msg');
+    });
+});
