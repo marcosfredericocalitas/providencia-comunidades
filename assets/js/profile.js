6c9f29ad3b0f613ec7102533bab7a3c1facc9ccc
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-10-05 15:59:05 +0100

    Validar  inputes pelo focusout

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+    // Preview selected image
+    previewImageBySelected("image", "image-preview");
+
+    // Validation by blur (Focusout event)
+    const validationsFunctionsProfile = [
+        // Add student functions and params
+        ["name", "name-err", validateName],
+        ["email", "email-err", validateEmail],
+        ["birthdate", "birthdate-err", validateBirthdate],
+        ["gender", "gender-err", validateGender],
+        ["phone", "phone-err", validatePhone]
+    ];
+
+    validationsFunctionsProfile.forEach((array) => {
+        validateByFocusOut(array[0], array[1], array[2]);
+    });
+
+    // Validation by click button (Click event)
     $("#bnt-edit-profile").click(function () {
         const isNameValid = validateName("name", "name-err");
         const isEmailValid = validateEmail("email", "email-err");
