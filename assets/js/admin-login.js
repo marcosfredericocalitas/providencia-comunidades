1e1ed731a9c2da938e986a045567359975feb093
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-10-05 01:46:24 +0100

    Validações do formulário de login

--- assets/js/admin-login.js
+++ assets/js/admin-login.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    alert('msg');
     // Validation by blur (Focusout event)
 
     // Validation by click button (Click event)
     $("#btn-login").click(function () {
-        alert('msg');
+        const isEmailValid = validateEmail("email", "email-err");
+        const isPasswordValid = validatePassword("password", "password-err");
     });
 });
