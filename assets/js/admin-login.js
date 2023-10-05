e1049b777fed32323804eb1b26d673d21f5c7ab7
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-10-05 02:11:16 +0100

    Login actualizado

--- assets/js/admin-login.js
+++ assets/js/admin-login.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
     // Validation by blur (Focusout event)
+    validateByFocusOut("email", "email-err", validateEmail);
+    validateByFocusOut("password", "password-err", validatePassword);
 
     // Validation by click button (Click event)
     $("#btn-login").click(function () {
         const isEmailValid = validateEmail("email", "email-err");
         const isPasswordValid = validatePassword("password", "password-err");
+
+        if (!isEmailValid || isPasswordValid) {
+            alert("Erro ao entrar");
+            return false;
+        }
+
+        alert('Logado com sucesso');
+        return true;
     });
 });
