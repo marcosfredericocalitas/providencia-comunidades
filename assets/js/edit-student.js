dcdcf02bc6b05d98492301e0f63dacab953aa24b
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-10-19 18:49:29 +0100

    Formulário do aluno actualizado

--- assets/js/edit-student.js
+++ assets/js/edit-student.js
@@ -1,26 +1,23 @@
 $(document).ready(function () {
-    // Preview selected image
-    previewImageBySelected("edit-image", "edit-image-preview");
-    // Validation by blur (Focusout event)
-    validateByFocusOut("edit-course", "edit-course-err", validateCourse);
+    setImagePreview("edit-image", "edit-image-preview");
 
     // Validation by click button (Click event)
     $("#btn-edit-student").click(function () {
         const isEditNameValid = validateName("edit-name", "edit-name-err");
         const isEditEmailValid = validateEmail("edit-email", "edit-email-err");
-        const isEditBirthDateValid = validateBirthdate("edit-birthdate", "edit-birthdate-err");
+        const isEditBirthdateValid = validateBirthdate("edit-birthdate", "edit-birthdate-err");
         const isEditGenderValid = validateGender("edit-gender", "edit-gender-err");
         const isEditCourseValid = validateCourse("edit-course", "edit-course-err");
         const isEditPhoneValid = validatePhone("edit-phone", "edit-phone-err");
-        const isValidEditImage = validateImage("edit-image", "edit-image-err");
+        const isEditImageValid = validateImage("edit-image", "edit-image-err");
 
-        if (!isEditNameValid || !isEditEmailValid || !isEditBirthDateValid || !isEditGenderValid ||
-            !isEditCourseValid || !isEditPhoneValid || !isValidEditImage) {
-            alert('Erro ao cadastrar aluno');
+        if (!isEditNameValid || !isEditEmailValid || !isEditBirthdateValid ||
+            !isEditGenderValid || !isEditCourseValid || !isEditPhoneValid || !isEditImageValid) {
+            alert('Erro ao adicioar novo aluno');
             return false;
         }
 
-        alert('Aluno cadastrado com sucesso');
+        alert('Aluno adicionado com sucesso');
         return true;
     });
 });
