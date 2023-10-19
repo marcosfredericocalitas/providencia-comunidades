a36c4247631e7511fd3350fe278e1445ecdbf376
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-10-19 21:00:10 +0100

    Chamada das funções aprimoradas

--- assets/js/add-admin.js
+++ assets/js/add-admin.js
@@ -1,26 +1,23 @@
 $(document).ready(function () {
-    // Preview selected image
-    previewImageBySelected("image", "image-preview");
-    // Validation by blur (Focusout event)
-    validateByFocusOut("password", "password-err", validatePassword);
+    setImagePreview("image", "image-preview");
 
     // Validation by click button (Click event)
     $("#btn-add-admin").click(function () {
-        const isValidName = validateName("name", "name-err");
-        const isValidEmail = validateEmail("email", "email-err");
-        const isValidImage = validateImage("image", "image-err");
-        const isBirthDateValid = validateBirthdate("birthdate", "birthdate-err");
-        const isValidGender = validateGender("gender", "gender-err");
-        const isValidPhone = validatePhone("phone", "phone-err");
-        const isValidPassword = validatePassword("password", "password-err");
+        const isNameValid = validateName("name", "name-err");
+        const isEmailValid = validateEmail("email", "email-err");
+        const isImageValid = validateImage("image", "image-err");
+        const isBirthdateValid = validateBirthdate("birthdate", "birthdate-err");
+        const isGenderValid = validateGender("gender", "gender-err");
+        const isPhoneValid = validatePhone("phone", "phone-err");
+        const isPasswordValid = validatePassword("password", "password-err");
 
-        if (!isValidName || !isValidEmail || !isValidImage || !isBirthDateValid
-            || !isValidGender || !isValidPhone || !isValidPassword) {
-            alert('Erro ao cadastrar admin');
+        if (!isNameValid || !isEmailValid || !isBirthdateValid ||
+            !isImageValid || !isGenderValid || !isPhoneValid || !isPasswordValid) {
+            alert('Erro ao adicioar novo admin');
             return false;
         }
 
-        alert('Admin cadastrado com sucesso');
+        alert('Admin adicionado com sucesso');
         return true;
-    })
-})
+    });
+});
--- assets/js/edit-admin.js
+++ assets/js/edit-admin.js
@@ -1,29 +1,23 @@
 $(document).ready(function () {
-    // Preview selected image
-    previewImageBySelected("edit-image", "edit-image-preview");
-    // Validation by blur (Focusout event)
-    validateByFocusOut("edit-password", "edit-password-err", validatePassword);
+    setImagePreview("edit-image", "edit-image-preview");
 
     // Validation by click button (Click event)
     $("#btn-edit-admin").click(function () {
-        const isValidEditName = validateName("edit-name", "edit-name-err");
-        const isValidEditEmail = validateEmail("edit-email", "edit-email-err");
-        const isValidEditImage = validateImage("edit-image", "edit-image-err");
-        const isEditBirthDateValid = validateBirthdate("edit-birthdate", "edit-birthdate-err");
-        const isValidEditGender = validateGender("edit-gender", "edit-gender-err");
-        const isValidEditPhone = validatePhone("edit-phone", "edit-phone-err");
-        const isValidEditPassword = validatePassword("edit-password", "edit-password-err");
+        const isEditNameValid = validateName("edit-name", "edit-name-err");
+        const isEditEmailValid = validateEmail("edit-email", "edit-email-err");
+        const isEditImageValid = validateImage("edit-image", "edit-image-err");
+        const isEditBirthdateValid = validateBirthdate("edit-birthdate", "edit-birthdate-err");
+        const isEditGenderValid = validateGender("edit-gender", "edit-gender-err");
+        const isEditPhoneValid = validatePhone("edit-phone", "edit-phone-err");
+        const isEditPasswordValid = validatePassword("edit-password", "edit-password-err");
 
-        if (!isValidEditName || !isValidEditEmail || !isValidEditImage || !isEditBirthDateValid ||
-            !isValidEditGender || !isValidEditPhone || !isValidEditPassword) {
-            alert('Erro ao editar admin');
+        if (!isEditNameValid || !isEditEmailValid || !isEditBirthdateValid ||
+            !isEditImageValid || !isEditGenderValid || !isEditPhoneValid || !isEditPasswordValid) {
+            alert('Erro ao adicioar novo admin');
             return false;
         }
 
-        alert('Admin editado com sucesso');
+        alert('Admin adicionado com sucesso');
         return true;
-    })
-
-    togglePasswordVisibility("#edit-btn-eye-open", "#password", "#edit-btn-eye-close");
-    togglePasswordVisibility("#edit-btn-eye-close", "#password", "#edit-btn-eye-open");
-})
+    });
+});
