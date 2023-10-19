ad2263a54a846d01d4012149ce676ce0bc68a4ff
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-10-19 16:11:02 +0100

    Validação do formulário actualizadaa

--- assets/js/add-student.js
+++ assets/js/add-student.js
@@ -1,26 +1,23 @@
 $(document).ready(function () {
-    // Preview selected image
-    previewImageBySelected("image", "image-preview");
-    // Validation by blur (Focusout event)
-    validateByFocusOut("course", "course-err", validateCourse);
+    setImagePreview("image", "image-preview");
 
     // Validation by click button (Click event)
     $("#btn-add-student").click(function () {
         const isNameValid = validateName("name", "name-err");
         const isEmailValid = validateEmail("email", "email-err");
-        const isBirthDateValid = validateBirthdate("birthdate", "birthdate-err");
+        const isBirthdateValid = validateBirthdate("birthdate", "birthdate-err");
         const isGenderValid = validateGender("gender", "gender-err");
-        const iscourseValid = validateCourse("course", "course-err");
+        const isCourseValid = validateCourse("course", "course-err");
         const isPhoneValid = validatePhone("phone", "phone-err");
-        const isValidImage = validateImage("image", "image-err");
+        const isImageValid = validateImage("image", "image-err");
 
-        if (!isNameValid || !isEmailValid || !isBirthDateValid || !isGenderValid ||
-            !iscourseValid || !isPhoneValid || !isValidImage) {
-            alert('Erro ao cadastrar aluno');
+        if (!isNameValid || !isEmailValid || !isBirthdateValid ||
+            !isGenderValid || !isCourseValid || !isPhoneValid || !isImageValid) {
+            alert('Erro ao adicioar novo aluno');
             return false;
         }
 
-        alert('Aluno cadastrado com sucesso');
+        alert('Aluno adicionado com sucesso');
         return true;
     });
 });
