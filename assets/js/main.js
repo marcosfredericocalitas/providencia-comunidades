b57282213d9ddff5ff2012f3aba50656c2d88b23
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-11-02 23:54:11 +0100

    Função de adicionar classes ao scrolar a tela criada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,25 @@
 // Add class by click
-function addClassByClick(button, domElement, className) {
+function addClassByClick(button, element, className) {
     const buttonId = document.getElementById(button);
-    const elementId = document.getElementById(domElement);
+    const elementId = document.getElementById(element);
 
     buttonId.addEventListener("click", () => {
         elementId.classList.toggle(className);
+    });
+}
+
+// Add class by window scroll
+function addClassByScroll(element, className, time) {
+    let windowScroll = window.scrollY;
+    const elementId = document.getElementById(element);
+    let shouldAddClass = windowScroll > time;
+
+    elementId.classList.toggle(className, shouldAddClass);
+
+}
+
+function addClassOnScrollEvent(element, classList, time) {
+    window.addEventListener("scroll", () => {
+        addClassByScroll(element, classList, time);
     });
 }
