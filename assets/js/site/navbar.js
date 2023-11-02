110f6895e4175c6140233ad100836a2247b6e7d1
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-11-02 23:53:04 +0100

    Função de adicionar classes ao scrolar a tela adicionada e chamada

--- assets/js/site/navbar.js
+++ assets/js/site/navbar.js
@@ -1,4 +1,5 @@
-const addClassesByScroll = [
+// Add class by button click
+const addClassesByClick = [
     { button: "btn-mobile", domElement: "menu-mobile", className: "is-block" },
     { button: "btn-mobile", domElement: "menu-searth", className: "is-block" },
     { button: "btn-mobile", domElement: "icon-menu-mobile-open", className: "is-none" },
@@ -6,6 +7,15 @@ const addClassesByScroll = [
     { button: "btn-mobile", domElement: "menu-mobile-wrapper", className: "is-flex" }
 ];
 
-for (let addClassOnClick of addClassesByScroll) {
+for (let addClassOnClick of addClassesByClick) {
     addClassByClick(addClassOnClick.button, addClassOnClick.domElement, addClassOnClick.className);
+}
+
+// Add class by window scroll
+const addClassesByScroll = [
+    { element: "navbar", className: "is-fixed", time: 100 }
+];
+
+for (let addClassOnScroll of addClassesByScroll) {
+    addClassOnScrollEvent(addClassOnScroll.element, addClassOnScroll.className, addClassOnScroll.time);
 }
