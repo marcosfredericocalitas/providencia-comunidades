7e9bc59403370b8fd06bd1a28abb64830f7eed93
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-11-02 19:25:08 +0100

    Adicionada chamada da função addClassByClick no menu wrapper

--- assets/js/site/navbar.js
+++ assets/js/site/navbar.js
@@ -1,8 +1,9 @@
 const addClasses = [
-    { button: "btn-mobile", domElement: "menu-mobile", className: "is-flex" },
-    { button: "btn-mobile", domElement: "menu-searth", className: "is-flex" },
+    { button: "btn-mobile", domElement: "menu-mobile", className: "is-block" },
+    { button: "btn-mobile", domElement: "menu-searth", className: "is-block" },
     { button: "btn-mobile", domElement: "icon-menu-mobile-open", className: "is-none" },
-    { button: "btn-mobile", domElement: "icon-menu-mobile-close", className: "is-none" }
+    { button: "btn-mobile", domElement: "icon-menu-mobile-close", className: "is-none" },
+    { button: "btn-mobile", domElement: "menu-mobile-wrapper", className: "is-flex" }
 ];
 
 for (let addClass of addClasses) {
