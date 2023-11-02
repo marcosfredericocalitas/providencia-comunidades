7da61c321867c7dba3ddd8bab319d2478213508d
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-11-03 00:00:13 +0100

    Propriedade domElement alterada para element

--- assets/js/site/navbar.js
+++ assets/js/site/navbar.js
@@ -1,14 +1,14 @@
 // Add class by button click
 const addClassesByClick = [
-    { button: "btn-mobile", domElement: "menu-mobile", className: "is-block" },
-    { button: "btn-mobile", domElement: "menu-searth", className: "is-block" },
-    { button: "btn-mobile", domElement: "icon-menu-mobile-open", className: "is-none" },
-    { button: "btn-mobile", domElement: "icon-menu-mobile-close", className: "is-none" },
-    { button: "btn-mobile", domElement: "menu-mobile-wrapper", className: "is-flex" }
+    { button: "btn-mobile", element: "menu-mobile", className: "is-block" },
+    { button: "btn-mobile", element: "menu-searth", className: "is-block" },
+    { button: "btn-mobile", element: "icon-menu-mobile-open", className: "is-none" },
+    { button: "btn-mobile", element: "icon-menu-mobile-close", className: "is-none" },
+    { button: "btn-mobile", element: "menu-mobile-wrapper", className: "is-flex" }
 ];
 
 for (let addClassOnClick of addClassesByClick) {
-    addClassByClick(addClassOnClick.button, addClassOnClick.domElement, addClassOnClick.className);
+    addClassByClick(addClassOnClick.button, addClassOnClick.element, addClassOnClick.className);
 }
 
 // Add class by window scroll
