77bb7e651a4d94aa62e4349bef50198777026831
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-11-02 01:41:05 +0100

    Visibilidade dos icones do botão mobile à funcionar

--- assets/js/site/navbar.js
+++ assets/js/site/navbar.js
@@ -1,6 +1,8 @@
 const addClasses = [
     { button: "btn-mobile", domElement: "menu-mobile", className: "is-flex" },
-    { button: "btn-mobile", domElement: "menu-searth", className: "is-flex" }
+    { button: "btn-mobile", domElement: "menu-searth", className: "is-flex" },
+    { button: "btn-mobile", domElement: "icon-menu-mobile-open", className: "is-none" },
+    { button: "btn-mobile", domElement: "icon-menu-mobile-close", className: "is-none" }
 ];
 
 for (let addClass of addClasses) {
