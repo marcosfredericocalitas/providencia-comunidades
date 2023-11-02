c86a7ce8bde84ce73088c698b83454bbbdaa6a0d
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-11-03 00:04:14 +0100

    Comentários alterados

--- assets/js/site/navbar.js
+++ assets/js/site/navbar.js
@@ -1,4 +1,4 @@
-// Add class by button click
+// Array of classes to be added on button click
 const classesToAddOnClick = [
     { button: "btn-mobile", element: "menu-mobile", className: "is-block" },
     { button: "btn-mobile", element: "menu-searth", className: "is-block" },
@@ -7,15 +7,17 @@ const classesToAddOnClick = [
     { button: "btn-mobile", element: "menu-mobile-wrapper", className: "is-flex" }
 ];
 
+// Add classes on button click
 for (let classToAddOnClick of classesToAddOnClick) {
     addClassByClick(classToAddOnClick.button, classToAddOnClick.element, classToAddOnClick.className);
 }
 
-// Add class by window scroll
+// Array of classes to be added on window scroll
 const classesToAddByScroll = [
     { element: "navbar", className: "is-fixed", time: 100 }
 ];
 
+// Add classes on window scroll
 for (let classToAddByScroll of classesToAddByScroll) {
     addClassOnScrollEvent(classToAddByScroll.element, classToAddByScroll.className, classToAddByScroll.time);
 }
