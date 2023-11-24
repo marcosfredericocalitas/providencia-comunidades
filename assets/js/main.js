366f785ab27c2a3878501516c67d1226b650bcec
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-11-24 15:48:50 +0100

    Função getWindowScrollY criada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+// Get window scroll Y  
+function getWindowScrollY() {
+    return {
+        windowScroll: window.scrollY
+    };
+};
+
 // Add a class on button click
 function addClassByClick(button, element, className) {
     const buttonId = document.getElementById(button);
@@ -6,16 +13,16 @@ function addClassByClick(button, element, className) {
     buttonId.addEventListener("click", () => {
         elementId.classList.toggle(className);
     });
-}
+};
 
 // Add a class on window scroll
 function addClassByScroll(element, className, time) {
-    const windowScroll = window.scrollY;
+    const { windowScroll } = getWindowScrollY();
     const shouldAddClass = windowScroll > time;
     const elementId = document.getElementById(element);
 
     elementId.classList.toggle(className, shouldAddClass);
-}
+};
 
 // Remove classes
 function removeClassBy(element, classNam) {
@@ -25,4 +32,4 @@ function removeClassBy(element, classNam) {
     if (windowWidth <= 1120) {
         elementId.classList.remove(classNam);
     }
-}
+};
