72744d870f336bfae5477c4ef9ea6fdae80d5ebc
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-11-28 13:19:54 +0100

    Funcionalidade dos controles next e prev adicionadas na função next image

--- assets/js/site/home.js
+++ assets/js/site/home.js
@@ -4,6 +4,28 @@ function nextImage() {
     let counter = 1;
     const totalImages = 4;
     let slider = document.getElementById("radio-1").checked = true;
+    const sliderControl = document.querySelectorAll(".slider__control");
+    let count;
+
+    sliderControl.forEach(controler => {
+        controler.addEventListener("click", () => {
+            count = controler.classList.contains("slider__control--prev") ? -1 : 1;
+
+            if (count == 1) {
+                counter++;
+            } else {
+                counter--;
+            }
+
+            if (counter > 4) {
+                counter = 1;
+            } else if (count < 1) {
+                counter = 4;
+            }
+
+            slider = document.getElementById("radio-" + counter).checked = true;
+        });
+    });
 
     setInterval(() => {
         counter++;
