1ac384bc1c1d9e39e529f3608f90f9fda106fcee
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-11-30 18:08:14 +0100

    classesToAddByEvent melhorada

--- assets/js/site/navbar.js
+++ assets/js/site/navbar.js
@@ -18,10 +18,20 @@ for (let classToAddOnClick of classesToAddByClick) {
 
 // Array of classes to be added on window scroll
 const classesToAddByEvent = [
-    { element: "navbar", className: "is-fixed", time: 300 },
-    { element: "navbar", className: "is-visible", time: 400 },
-    { element: "menu-logo", className: "is-small", time: 300 },
-    { element: "btn-scroll-top", className: "is-visible", time: 400 }
+    // Card template
+    { element: "card-details", className: "is-fixed", time: 195, type: "not-full" },
+    { element: "card-details", className: "has-transition", time: 200, type: "not-full" },
+    { element: "card-details", className: "is-top", time: 400, type: "not-full" },
+    { element: "card-details", className: "has-no-transition-2", time: 650, type: "not-full" },
+    { element: "card-details", className: "is-not-fixed", time: 1568, type: "not-full" },
+    { element: "section-content", className: "is-full", time: 1568, type: "not-full" },
+    { element: "main-item", className: "has-gap", time: 1568, type: "not-full" },
+    { element: "main-content-course", className: "u-relative", time: 1568, type: "not-full" },
+    // Navbar
+    { element: "navbar", className: "is-fixed", time: 300, type: "full" },
+    { element: "navbar", className: "is-visible", time: 400, type: "full" },
+    { element: "menu-logo", className: "is-small", time: 300, type: "full" },
+    { element: "btn-scroll-top", className: "is-visible", time: 400, type: "full" }
 ];
 
 // Add classes on window scroll
@@ -30,23 +40,11 @@ window.addEventListener("scroll", () => {
         addClassByScroll(
             classToAddByEvent.element,
             classToAddByEvent.className,
-            classToAddByEvent.time
+            classToAddByEvent.time,
+            classToAddByEvent.type
         );
     };
 });
-
-// Remove class by event
-function removeClassByEvent(event) {
-    window.addEventListener(event, () => {
-        for (let i = 0; i < 3; i++) {
-            let classToAddByEvent = classesToAddByEvent[i];
-            removeClassBy(classToAddByEvent.element, classToAddByEvent.className);
-        }
-    });
-};
-
-removeClassByEvent("resize");
-removeClassByEvent("scroll");
 
 //Scroll the window to the top by button click
 function scrollToTop() {
