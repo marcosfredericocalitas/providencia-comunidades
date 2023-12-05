48282a15a54ea70a533e732a29a360fcbac0054a
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-12-05 05:07:24 +0100

    Slider de noticias aidcionado (Home)

--- assets/js/site/home.js
+++ assets/js/site/home.js
@@ -1,36 +1,37 @@
 // Next image 
-function nextImage() {
+function nextImage(radio) {
     let counter = 1;
     const totalImages = 4;
-    let slider = document.getElementById("radio-1").checked = true;
-    const sliderControl = document.querySelectorAll(".slider__control");
-    let count;
-
-    sliderControl.forEach(controler => {
-        controler.addEventListener("click", () => {
-            count = controler.classList.contains("slider__control--prev") ? -1 : 1;
-            if (count == 1) {
-                counter++;
-            } else {
-                counter--;
-            }
-
-            if (counter > 4) {
-                counter = 1;
-            } else if (count < 1) {
-                counter = 4;
-            }
-            slider = document.getElementById("radio-" + counter).checked = true;
-        });
-    });
+    let slider = document.getElementById(radio + "-1").checked = true;
+    // const sliderControl = document.querySelectorAll(".slider__control");
+    // let count;
 
     setInterval(() => {
         counter++;
         if (counter > totalImages) {
             counter = 1;
         }
-        slider = document.getElementById("radio-" + counter).checked = true;
+        slider = document.getElementById(radio + "-" + counter).checked = true;
     }, 3000);
 };
 
-nextImage();
+nextImage("radio");
+nextImage("news-radio");
+
+// sliderControl.forEach(controler => {
+//     controler.addEventListener("click", () => {
+//         count = controler.classList.contains("slider__control--prev") ? -1 : 1;
+//         if (count == 1) {
+//             counter++;
+//         } else {
+//             counter--;
+//         }
+
+//         if (counter > 4) {
+//             counter = 1;
+//         } else if (count < 1) {
+//             counter = 4;
+//         }
+//         slider = document.getElementById("radio-" + counter).checked = true;
+//     });
+// });
