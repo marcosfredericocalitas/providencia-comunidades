1558b7936c1d8dc00b51efc0c80f89598f68a85e
Marcos Frederico Calitas <124792684+marcosfredericocalitas@users.noreply.github.com>
2023-12-08 19:54:28 +0100

    course-template.js adicionado

---
+++ assets/js/site/course-template.js
@@ -0,0 +1,5 @@
+
+// { button: "btn-signup-course", element: "modal-container", className: "is-visible" },
+// { button: "btn-hide-signup-course", element: "modal-container", className: "is-visible" },
+addClassByClick("btn-signup-course", "modal-container", "is-visible");
+addClassByClick("btn-hide-signup-course", "modal-container", "is-visible");
