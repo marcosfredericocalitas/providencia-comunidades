fd058b64c1209a1d3e40dfea3b25812ba58a5fa8
Marcos Frederico Calitas <marcosfredericocalitas22@gmail.com>
2024-01-06 21:48:14 +0100

    Arquivo signup.js adicionado

---
+++ assets/js/site/signup.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    // Set image preview
+    setImagePreview("image", "image-preview");
+    // Add class by click
+    addClassByClick("btn-signup-course", "modal-container", "is-visible");
+    addClassByClick("btn-hide-signup-course", "modal-container", "is-visible")
+
+    // Validation by focusout event
+    const signupValidationFields = [
+        // Add student functions and params
+        ["name", "name-err", validateName],
+        ["email", "email-err", validateEmail],
+        ["birthdate", "birthdate-err", validateBirthdate],
+        ["phone", "phone-err", validatePhone],
+        ["image", "image-err", validateImage],
+    ];
+
+    validateFieldsOnFocusOut(signupValidationFields);
+
+    // Validation by click button
+    $("#btn-signup-student").click(function () {
+        const isNameValid = validateName("name", "name-err");
+        const isEmailValid = validateEmail("email", "email-err");
+        const isBirthdateValid = validateBirthdate("birthdate", "birthdate-err");
+        const isGenderValid = validateGender("gender", "gender-err");
+        const isPhoneValid = validatePhone("phone", "phone-err");
+        const isImageValid = validateImage("image", "image-err");
+
+        if (!isNameValid || !isEmailValid || !isBirthdateValid ||
+            !isGenderValid || !isPhoneValid || !isImageValid) {
+            return false;
+        }
+
+        $("#modal-container").removeClass("is-visible");
+        setTimeout(() => {
+            showMessageSignupedsuccessfully();
+        }, 150);
+        return true;
+    });
+});
